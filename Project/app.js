f036dae931c49f8783f6ac3e11d27f643014993e
aideisayevaa <aideisayeva3@gmail.com>
2023-01-25 09:32:28 +0400

    update

--- Project/app.js
+++ Project/app.js
@@ -1,5 +1,5 @@
 let section = document.querySelectorAll('section');
-let lists = document.querySelectorAll('.nav__list');
+let lists = document.querySelectorAll('.nav__list a');
 function activeLink(li) {
     lists.forEach((item) => item.classList.remove('active'));
     li.classList.add('active');
@@ -9,16 +9,19 @@ lists.forEach((item) =>
         activeLink(this);
     }));
 
-// window.onscroll = () => {
-//     section.forEach((sec) => {
-//         let top = window.scrollY;
-//         let offset = sec.offsetTop;
-//         let height = sec.offsetHeight;
-//         let id = sec.getAttribute('id');
+window.onscroll = () => {
+    section.forEach((sec) => {
+        let top = window.scrollY;
+        let offset = +sec.offsetTop - 200;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-//         if (top >= offset && top < offset + height) {
-//             const target = document.querySelector(`[href='#${id}']`).parentElement;
-//             activeLink(target);
-//         }
-//     })
-// };
+        if (top >= offset && top < offset + height) {
+            const target = document.querySelector(`[href='#${id}']`);
+            activeLink(target);
+        }
+    })
+};
+
+
+
